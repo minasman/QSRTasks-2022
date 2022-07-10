f3b296802b8f1eff8dad0355b53a6dfe5989f993
minasman <elreymcd@yahoo.com>
2022-07-10 14:25:40 -0500

    document

--- app/controllers/documentations/documentations_controller.rb
+++ app/controllers/documentations/documentations_controller.rb
@@ -23,6 +23,10 @@ class Documentations::DocumentationsController < ApplicationController
   # POST /documentations or /documentations.json
   def create
     @documentation = Documentation.new(documentation_params)
+    new_params = params.merge!(
+      organization_id = current_user.organization.id,
+
+    )
 
     respond_to do |format|
       if @documentation.save
@@ -100,6 +104,78 @@ class Documentations::DocumentationsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def documentation_params
-      params.require(:documentation).permit(:organization_id, :store_id, :employee_named_id, :awarded_by_id, :position_id, :document_id, :documentation_type, :level, :documentation_class, :description, :points, :document_date, :individiual, :document_description)
+      params.require(:documentation).permit(:organization_id, :store_id, :employee_named_id, :awarded_by_id, :position_id, :document_id, :documentation_type, :level, :documentation_class, :description, :points, :document_date, :individual, pictures: [])
+    end
+
+    def flow_of_accountability(document, new_params)
+      named_employee = User.find(document.employee_named_id)
+      department = document.position.department
+      position = document.position.name
+      if department == "Operations"
+        if position == "Crew" || position == "Manager"
+          flow_list = named_employee.stores[0].users.where(position_id: Position.where(name: ["General Manager", "Supervisor", "Operations Manager", "Director"]).ids)
+        elsif position == "General Manager"
+          flow_list = named_employee.stores[0].users.where(position_id: Position.where(name: ["Supervisor", "Operations Manager", "Director"]).ids)
+        elsif position == "Supervisor"
+          flow_list = named_employee.stores[0].users.where(position_id: Position.where(name: ["Operations Manager", "Director"]).ids)
+        else #Operations Manager
+          flow_list = named_employee.stores[0].users.where(position_id: Position.where(name: ["Director"]).ids)
+        end
+
+      elsif department == "Maintenance"
+        if position == "Maint Admin"
+          flow_list = User.where(position_id: Position.where(name: ["Maint Department Head"]).ids)
+        elsif position == "AA"
+          flow_list = User.where(position_id: Position.where(name: ["Maint Department Head"]).ids)
+        elsif position == "Maint Tech"
+          flow_list = User.where(position_id: Position.where(name: ["Maint Tech Department Head", "Maint Department Head"]).ids)
+        elsif position == "OTP Tech"
+          flow_list = User.where(position_id: Position.where(name: ["Technology Department Head", "Maint Department Head"]).ids)
+        elsif position == "Patch Maint"
+          flow_list = User.where(position_id: Position.where(name: ["PM Department Head", "Maint Department Head"]).ids)
+        elsif position == "PM Department Head"
+          flow_list = User.where(position_id: Position.where(name: ["Maint Department Head"]).ids)
+        elsif position == "Maint Tech Department Head"
+          flow_list = User.where(position_id: Position.where(name: ["Maint Department Head"]).ids)
+        elsif position == "Technology Department Head"
+          flow_list = User.where(position_id: Position.where(name: ["Maint Department Head"]).ids)
+        else # Maint Department Head (Need logic to not duplicate when DM is doc)
+          flow_list = User.where(position_id: Position.where(name: [""]).ids)
+        end
+
+      else
+        if position == "HR Office Admin"
+          flow_list = User.where(position_id: Position.where(name: ["HR Manager", "Business Director"]).ids)
+        elsif position == "HR Manager"
+          flow_list = User.where(position_id: Position.where(name: ["Business Director"]).ids)
+        elsif position == "Payroll Office Admin"
+          flow_list = User.where(position_id: Position.where(name: ["Payroll Manager", "Business Director"]).ids)
+        elsif position == "Shopper"
+          flow_list = User.where(position_id: Position.where(name: ["Maint Department Head"]).ids)
+        elsif position == "AP Office Admin"
+          flow_list = User.where(position_id: Position.where(name: ["AP Manager", "Business Director"]).ids)
+        elsif position == "AR Office Admin"
+          flow_list = User.where(position_id: Position.where(name: ["AR Manager", "Business Director"]).ids)
+        elsif position == "Marketing Office Admin"
+          flow_list = User.where(position_id: Position.where(name: ["Marketing Manager", "Business Director"]).ids)
+        elsif position == "AP Manager"
+          flow_list = User.where(position_id: Position.where(name: ["Business Director"]).ids)
+        elsif position == "AR Manager"
+          flow_list = User.where(position_id: Position.where(name: ["Business Director"]).ids)
+        elsif position == "Marketing Manager"
+          flow_list = User.where(position_id: Position.where(name: ["Business Director"]).ids)
+        elsif position == "Payroll Manager"
+          flow_list = User.where(position_id: Position.where(name: ["Business Director"]).ids)
+        else #Business Director
+          flow_list = User.where(position_id: Position.where(name: [""]).ids)
+        end
+      end
+      flow_list.each do |employee|
+        flow_document = Document.new(new_params)
+        flow_document.employee_named = employee
+        flow_document.position = employee.position
+        flow_document.description = "Initial Named Employee: #{document.employee_named.full_name} at #{document.store.number}: #{document.description}"
+        flow_document.save
+      end
     end
 end
