3ca6ec77a80e330abe0b256c1c5dcfc555b2a40b
minasman <elreymcd@yahoo.com>
2022-07-10 13:02:37 -0500

    update documentation model

--- app/models/documentations/documentation.rb
+++ app/models/documentations/documentation.rb
@@ -5,4 +5,18 @@ class Documentation < ApplicationRecord
   belongs_to :awarded_by
   belongs_to :position
   has_one :document
+  has_many_attached :pictures, dependent: :delete_all
+
+  validate :acceptable_images
+
+  def acceptable_images
+    return unless pictures.attached?
+    acceptable_types = ["image/jpeg", "image/png"]
+    pictures.each do |p|
+      unless acceptable_types.include?(p.content_type)
+        errors.add(:pictures, "Pictures Must Be JPEG or PNG")
+      end
+    end
+  end
+
 end
